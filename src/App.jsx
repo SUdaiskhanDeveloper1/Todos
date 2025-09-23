d6798ee9325ac73f45b99db0b967b0bbc021eb0c
Sudaiskhan <engineersudais786@gmail.com>
2025-09-23 14:20:53 +0500

    width

--- src/App.jsx
+++ src/App.jsx
@@ -127,7 +127,7 @@ const TodoApp = () => {
   );
 
   return (
-    <Flex direction="column" minHeight="100vh" overflow="hidden" bg={bgColor}>
+    <Flex direction="column" minHeight="90vh" overflow="hidden" bg={bgColor}>
       <Box flex="1" maxW="800px" w="100%" mx="auto" px={4} py={4}>
         <Flex justify="space-between" align="center" mb={6}>
           <Heading as="h1" size="xl" textAlign={["center", "left"]}>
